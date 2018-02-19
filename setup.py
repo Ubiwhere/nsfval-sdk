087a3043478fc38fa43b6c2ad8f3824028ab00bf
Luís Conceição <lconceicao@ubiwhere.com>
2018-02-19 13:37:22 +0000

    init sdk dev

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='nsfval-sdk',
+    license='Apache License, Version 2.0',
+    version='1.1',
+    author='Luís Conceição',
+    author_email='lconceicao@ubiwhere.com',
+    packages=find_packages(),
+)
